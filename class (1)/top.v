5961c1474f8f77ab5e8766289b9d070f293d89b8
DanielDayan-Tera <danieldayan@u.boisestate.edu>
2026-03-12 10:44:06 -0600

    Refactor internet_valve module for clarity

--- class (1)/top.v
+++ class (1)/top.v
@@ -6,11 +6,11 @@ module internet_valve(
     output [15:0] led    
 );
 
-    wire mux_sel   = {btnU, btnL}; 
-    wire demux_sel = {btnR, btnD}; 
+    wire [1:0] mux_sel   = {btnU, btnL}; 
+    wire [1:0] demux_sel = {btnR, btnD}; 
 
 // condition ? true : false
-    wire mux_out;
+    wire [3:0] mux_out;
     assign mux_out = (mux_sel == 2'b00) ? sw[3:0]   : // CEO
                      (mux_sel == 2'b01) ? sw[7:4]   : // You
                      (mux_sel == 2'b10) ? sw[11:8]  : // Fred
@@ -26,3 +26,4 @@ module internet_valve(
     assign led[15:12] = (btnC && demux_sel == 2'b11) ? mux_out : 4'b0000;
 
 endmodule
+
